0676e008336984440668a06dbfe5d03161b19cad
godsangin <leein3293@gmail.com>
2019-06-10 17:32:23 +0900

    react movie app completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import fetchJsonp from 'fetch-jsonp';
 import './App.css';
 import Movie from './Movie';
+import jQuery from 'jquery';
+
 
 
 class App extends Component {
   state={
-
+    limit:20
   }
   componentWillMount(){
     console.log("will mount");
+    window.$ = window.jQuery = jQuery;
   }
   
   componentDidMount(){
-   this._getMovies();
+   this._getMovies(20);
+   window.addEventListener('scroll', this._infinitiScroll, true)
   }
 
   _renderMovies = () =>{
@@ -29,19 +33,38 @@ class App extends Component {
     return movies
   }
 
-  _getMovies = async () => {
-    const movies = await this._callApi()
+  _getMovies = async (newlimit) => {
+    const movies = await this._callApi(newlimit)
     this.setState({
+      limit:newlimit,
       movies
     })
   }
 
-  _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+  _callApi = (limit) => {
+    return fetch('http://yts.lt/api/v2/list_movies.jsonp?sort_by=download_count&limit='+limit)
     .then(response => response.json())
     .then(json => json.data.movies)
     .catch(err => console.log(err))
+
   }
+
+  _infinitiScroll = () => {
+    let scrollHeight= Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+    let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    let clientHeight = document.documentElement.clientHeight;
+
+    if(scrollTop + clientHeight === scrollHeight && this.state.limit < 50){
+      this._getMovies(this.state.limit + 10)
+    }
+  }
+  _validateResponse(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+  
 
   render(){
     const {movies} = this.state;
